1298f4954d8596c1eb379245b3398a8319196517
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-02-12 23:43:06 +0530

    Time: 34 ms (52.57%), Space: 13.8 MB (49.19%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,12 +1,16 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        arr = [i[-1] + i[:-1] for i in s.split()]
+        #"is2 sentence4 This1 a3"
+        #is2   2+is 2is
+        arra = [i[-1] + i[:-1] for i in s.split()]
         
-        arr.sort()
+        arra.sort()
         
-        ans = ""
-        for i in arr:
-            ans += i[1:] + ' '
+        ans=""
+        for i in arra:
+            ans += i[1:] + " "
         
         return ans[:-1]
+    
+            
         
