1872d335b3b7e195be578bdd4a55ce4bde232590
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-02-14 22:38:19 +0530

    Time: 119 ms (34.36%), Space: 13.8 MB (96.11%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,9 +1,12 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        from itertools import combinations, accumulate
-
+        from itertools import combinations,accumulate
+        
         res = 0
-        for i in range(1, len(nums)+1):  # Iterate through all lengths of subsets
-            for arr in combinations(nums, i):  # Sum up the XOR results
-                res += list(accumulate(arr, func=lambda x, y: x ^ y))[-1]
+        #[1,2,3,4]
+        for i in range(1,len(nums)+1):
+            for arr in combinations(nums,i):
+                res += list(accumulate(arr,func = lambda x,y : x^y))[-1]
         return res
+
+        
