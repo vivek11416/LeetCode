6642a6ccff5707ac5868f0dfdc7234f19ee9715d
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-08-10 13:22:59 +0530

    Time: 4095 ms (26.92%), Space: 24 MB (90.87%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -1,16 +1,19 @@
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
-
         freq = Counter(deliciousness)
         ans = 0
-
-        # for each number find its num2 so that num+num2 makes a power of 2
+        
         for num in deliciousness:
-            freq[num]-=1
+            freq[num] -= 1
             for i in range(22):
                 num2 = (2**i)-num
-
-                # if num2 happens to be in our original list, get its freq
+                
                 if num2 in freq:
                     ans += freq[num2]
+        
         return ans % (10**9+7)
+    
+
+                    
+            
+        
