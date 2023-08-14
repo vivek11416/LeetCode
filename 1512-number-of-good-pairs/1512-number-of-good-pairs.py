ec42ba13fd32c56dd567ad2fa8af605f9b0999e1
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-08-14 15:44:35 +0530

    Time: 40 ms (85.74%), Space: 16.3 MB (29.70%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -4,6 +4,7 @@ class Solution:
         ans = 0
         for num in nums:
             if num in gpHashmap:
+                
                 ans += gpHashmap[num]
                 gpHashmap[num] += 1
             else:
