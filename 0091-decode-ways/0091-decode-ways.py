38a0c65bcc48b5cc9547cdce884d2829e4f9d98b
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-08-22 22:05:51 +0530

    Time: 39 ms (79.92%), Space: 16.4 MB (32.62%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,25 +1,27 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0': 
+        if not s or s[0]=='0':
             return 0
+        
         n = len(s)
-        dp = [0] * (n)
-        dp[0]= 1
-        for i in range(1, n):
-            if s[i-1] == "0" and s[i] == "0":
+        dp = [0] * n
+        dp[0] = 1
+        
+        for i in range(1,n):
+            if s[i-1]=="0" and s[i] == "0":
                 dp[i] = 0
-            elif s[i-1] == "0" and s[i] != "0":
-                dp[i] = dp[i-1]
-            elif s[i-1] != "0" and s[i] == "0":
+            elif s[i-1]=="0" and s[i] != "0":
+                dp[i] = dp [i-1]
+            elif s[i-1] !="0" and s[i] == "0":
                 if s[i-1] == "1" or s[i-1] == "2":
                     dp[i] = dp[i-2] if i>=2 else 1
                 else:
                     dp[i] = 0
             else:
                 if int(s[i-1:i+1])<27:
-
-                    dp[i] = dp [i-1] + (dp[i-2] if i>=2 else 1)
+                    dp[i] = dp[i-1]+ (dp[i-2] if i>=2 else 1)
+                
                 else:
-                    dp[i] = dp [i-1]
-
+                    dp[i] = dp[i-1]
+                    
         return dp[n-1]
