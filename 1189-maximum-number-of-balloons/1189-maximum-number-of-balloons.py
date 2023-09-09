85783951bca9d3b6352ec7a88d0480d658e177c8
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-09-09 12:13:35 +0530

    Time: 54 ms (10.95%), Space: 16.5 MB (26.90%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = {"b":0,"a":0,"l":0,"o":0,"n":0}
+        textList = list(text)
+        temp = []
+        
+        for val in textList:
+            try:
+                count[val] +=1
+            except:
+                continue
+        
+        for k,v in count.items():
+            if k == "l" or k=="o":
+                temp.append(v//2)
+            else:
+                temp.append(v)
+        
+        print(temp)
+        print(count)
+                
+        return min(temp)
+                
+        
