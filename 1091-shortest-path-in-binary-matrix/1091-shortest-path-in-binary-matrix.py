9ea68304cee02cd370b15d53402fcba3e4b41ae9
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-09-09 21:58:21 +0530

    Time: 609 ms (44.90%), Space: 16.8 MB (66.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,33 +1,31 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # Edge case: impossible bc start or end cell isn't clear
-        if grid[0][0] or grid[-1][-1]:
+        
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
-        # Edge case: single cell
+        
         if grid == [[0]]:
             return 1
-
-        # (x, y, path length)
-        q = [(0, 0, 1)]
-        # Mark start as visited
+        
+        q= [(0,0,1)]
+        
         grid[0][0] = 1
-
-        # BFS
+        
         while q:
-            x, y, length = q.pop(0)
-
-            # Check neighboring cells
-            for nextX in [x-1, x, x+1]:
-                for nextY in [y-1, y, y+1]:
-                    # Answer found
+            x,y,length = q.pop(0)
+            
+            for nextX in [x-1,x,x+1]:
+                for nextY in [y-1,y,y+1]:
                     if nextX == len(grid) - 1 and nextY == len(grid[0]) - 1:
                         return length + 1
                     
-                    # Add next coordinate to next queue if the cell is clear
-                    if 0 <= nextX and nextX < len(grid) and 0 <= nextY and nextY < len(grid[0]):
+                    if nextX >= 0 and nextX < len(grid) and nextY >= 0 and nextY < len(grid[0]):
                         if grid[nextX][nextY] == 0:
-                            q.append((nextX, nextY, length + 1))
-                            # Mark as visited
+                            q.append((nextX,nextY,length+1))
                             grid[nextX][nextY] = 1
-
-        return -1
+                            
+        return -1      
+        
+        
+        
+        
