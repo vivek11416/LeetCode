255166fbc119df823edc200a285f2ea0db02a57d
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-09-10 22:59:03 +0530

    Time: 249 ms (24.51%), Space: 26.1 MB (89.22%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,24 +1,24 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        count = {}
+        
+        count= {}
         ans = 0
+        
         for val in dominoes:
             if str(val) in count or str(val[::-1]) in count:
                 try:
-                    count[str(val)]+=1
+                    count[str(val)] += 1
+                    
                 except:
                     try:
-                        count[str(val[::-1])]+=1 
+                        count[str(val[::-1])] += 1
                     except:
                         pass
-            
+                    
             else:
                 count[str(val)] = 1
-        
-        print(count)
+                
         for _,v in count.items():
             ans += (v*(v-1))//2
-            
         return ans
-            
-        
+                    
