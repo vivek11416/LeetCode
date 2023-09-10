6614cedacbc0b29f07be9eda4376886098759117
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-09-10 22:41:11 +0530

    Time: 47 ms (33.44%), Space: 16.3 MB (64.70%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -2,11 +2,11 @@ class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         count = {"b":0,"a":0,"l":0,"o":0,"n":0}
         textList = list(text)
-        temp = []
+        temp=[]
         
         for val in textList:
             try:
-                count[val] +=1
+                count[val] += 1
             except:
                 continue
         
@@ -15,8 +15,9 @@ class Solution:
                 temp.append(v//2)
             else:
                 temp.append(v)
-
                 
         return min(temp)
                 
+                
+        
         
