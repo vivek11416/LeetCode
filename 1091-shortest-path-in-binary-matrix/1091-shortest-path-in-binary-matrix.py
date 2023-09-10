1f3d0da683ba1adcf99ae8dd1347da7b848f719e
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2023-09-10 22:24:14 +0530

    Time: 594 ms (50.57%), Space: 16.6 MB (98.37%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -7,25 +7,23 @@ class Solution:
         if grid == [[0]]:
             return 1
         
-        q= [(0,0,1)]
+        q = [(0,0,1)]
         
         grid[0][0] = 1
         
-        while q:
-            x,y,length = q.pop(0)
+        
+        while q :
+            x ,y ,length = q.pop(0)
             
             for nextX in [x-1,x,x+1]:
                 for nextY in [y-1,y,y+1]:
-                    if nextX == len(grid) - 1 and nextY == len(grid[0]) - 1:
+                    if nextX == len(grid) -1 and nextY ==len(grid[0]) - 1:
                         return length + 1
                     
                     if nextX >= 0 and nextX < len(grid) and nextY >= 0 and nextY < len(grid[0]):
                         if grid[nextX][nextY] == 0:
                             q.append((nextX,nextY,length+1))
                             grid[nextX][nextY] = 1
-                            
-        return -1      
-        
-        
-        
-        
+                        
+        return -1
+                        
