b0091649bbc6ebbefd20fcc9a993d23d15c1f5b4
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-02-05 22:04:00 +0530

    Time: 82 ms (61.87%), Space: 19.3 MB (92.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,38 +5,48 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists)==0:
+        
+        if not lists or len(lists) ==0:
             return None
         
-        while len(lists) >1 :
+        
+        while len(lists) > 1 :
             mergedLists = []
-            for i in range(0,len(lists),2):
+            
+            for i in range ( 0,len(lists),2):
                 l1 = lists[i]
                 l2 = lists[i+1] if (i+1) < len(lists) else None
+                
                 mergedLists.append(self.mergeTwoLists(l1,l2))
             lists = mergedLists
+            
         return lists[0]
-                
-    
-    
+        
+        
+        
+        
     def mergeTwoLists(self,l1,l2):
         dummy = ListNode()
         head = dummy
-        
+
         while l1 and l2:
-            if l1.val < l2.val:
+            if l1.val <l2.val:
                 head.next = l1
                 l1 = l1.next
+
             else:
                 head.next = l2
                 l2 = l2.next
-                
+
             head = head.next
-        
+
+
         if l1:
             head.next = l1
-        
+
         if l2:
             head.next = l2
-            
-        return dummy.next  
+
+        return dummy.next
+
+        
