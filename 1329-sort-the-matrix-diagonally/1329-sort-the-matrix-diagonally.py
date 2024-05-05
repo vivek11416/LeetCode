993c0ec284a56a22faba106322ce5cdb989236bc
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-05 10:55:56 +0530

    Time: 68 ms (75.34%), Space: 16.8 MB (96.17%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,19 +1,18 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        maxCol =len(mat[0])
-        maxRow = len(mat)
+        self.maxCol =len(mat[0])
+        self.maxRow = len(mat)
         
         diagInd  = []
         
-        for i  in range(maxCol):
+        for i  in range(self.maxCol):
             diagInd.append([0,i])
             
-        for i  in range(maxRow):
+        for i  in range(self.maxRow):
             diagInd.append([i,0])
             
            
         for val in diagInd:
-            print(val)
             addR,val = self.return_sorted(val,mat)
             for i,v in enumerate(addR):
                 mat[v[0]][v[1]] = val[i]
@@ -22,17 +21,15 @@ class Solution:
             
         
     def return_sorted(self,diagStart,mat):
-        maxCol =len(mat[0])
-        maxRow = len(mat)
         diag_add  = [diagStart]
         sortedVals = [mat[diagStart[0]][diagStart[1]]]
         row  = diagStart[0]
         col = diagStart[1]
 
-        for i in range(diagStart[0],maxRow-1):
+        for i in range(diagStart[0],self.maxRow-1):
             row += 1
             col += 1
-            if  row<=maxRow-1 and col<=maxCol-1:
+            if  row<=self.maxRow-1 and col<=self.maxCol-1:
                 diag_add.append([row,col])
                 sortedVals.append(mat[row][col])
         x = sorted(sortedVals)    
