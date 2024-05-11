678cd1fb2d1df0f6471c6f258c13cf08d02d1dcc
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-12 00:44:10 +0530

    Time: 152 ms (71.81%), Space: 18.9 MB (78.93%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,23 +1,25 @@
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        n=len(quality)
-        worker=[[w/q, q] for w, q in zip(wage, quality)]
+        allRatios = [[wage[i]/quality[i],quality[i]] for i in range(len(quality))]
+        allRatios = sorted(allRatios)
+        ans = float('inf')
+        tot_sum = 0
+        max_heap = []
+        
+        for r,q in allRatios:
+            heappush(max_heap,-q)
+            tot_sum += q
+            
+            if len(max_heap) >k:
+                tot_sum += heappop(max_heap)
+            
+            if len(max_heap) ==k:
+                ans = min(ans , tot_sum*r)
+                
+        return ans
+            
+            
 
-        worker.sort()
-        quality_sum=0
-        heap=[]
-
-        for ratio, q in worker[:k]:
-            quality_sum+=q
-            heappush(heap, -q)
-        pay=worker[k-1][0]*quality_sum
-
-        for ratio, q in worker[k:n]:
-            heappush(heap, -q)
-            quality_sum+=q+heappop(heap)
-            pay=min(pay, quality_sum*ratio)
-
-        return pay
         
         
         
