fa5cb6b9f9cf1792c46a9fe3438fa8eb2bf87896
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-12 00:44:30 +0530

    Time: 169 ms (18.69%), Space: 19 MB (52.82%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -17,46 +17,7 @@ class Solution:
                 ans = min(ans , tot_sum*r)
                 
         return ans
-            
-            
-
-        
-        
-        
-        
-        
-        
-#         import itertools
-# #         allComb = []
-# #         for comb in itertools.permutations([i for i in range(len(quality))], k):
-# #             allComb.append(comb)
-            
-# #         # for  val  in allComb:
-# #         #     allComb.append(val[::-1])
-            
-#         minPrice=float('inf')  
-
-#         for val in itertools.permutations([i for i in range(len(quality))], k):
-#             ratio = wage[val[0]]/quality[val[0]]
-#             totalPrice = wage[val[0]]
-            
-#             for i in range(1,len(val)):
-#                 if wage[val[i]]<=quality[val[i]]*ratio:
-
-#                     totalPrice+=quality[val[i]]*ratio
-                    
-#                 else:
-#                     totalPrice  +=float('inf')  
-            
-#             # print( min(minPrice,totalPrice))
-#             minPrice= min(minPrice,totalPrice)
-            
-#         return minPrice
-                    
-                
-                
-                
-                
+   
             
             
         
