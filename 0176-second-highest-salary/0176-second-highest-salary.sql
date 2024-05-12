72d028a109ba4bd6e4ae7e812878df9b23ba09de
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-12 22:21:33 +0530

    Time: 593 ms (32.20%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,12 @@
+SELECT 
+    IFNULL(
+        (SELECT DISTINCT salary 
+        FROM
+            employee
+        ORDER BY 
+            salary DESC
+        LIMIT 1
+        OFFSET 1 ), NULL
+
+    )
+as SecondHighestSalary
