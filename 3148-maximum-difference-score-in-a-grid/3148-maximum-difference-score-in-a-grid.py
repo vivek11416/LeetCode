f72836729fe742501e1b93b64101ef1d183af9e4
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-13 22:34:46 +0530

    Time: 1197 ms (100.00%), Space: 29.8 MB (100.00%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
-        ans = -inf
         m = len(grid)
         n = len(grid[0])
+        ans = -inf
         dp = []
-        for i in range(m):
+        for _ in range(m):
             dp.append([-inf]*n)
+            
         dp[0][0] = 0
-        
         
         for i in range(m):
             for j in range(n):
+                
                 if i==0 and j==0:
                     continue
                 
@@ -18,13 +19,14 @@ class Solution:
                     c2 = grid[i][j]
                     c1 = grid[i][j-1]
                     optimalPathFromLeft = dp[i][j-1]
-                    dp[i][j] = max(c2-c1,c2-c1+optimalPathFromLeft )
+                    dp[i][j] = max(c2-c1,c2-c1+optimalPathFromLeft)
                     
                 if i-1 >= 0:
                     c2 = grid[i][j]
                     c1 = grid[i-1][j]
-                    # optimalPathFromLeft = dp[i-1][j]
-                    optimalPathFromup = dp[i-1][j]
-                    dp[i][j] = max(dp[i][j],c2-c1,c2-c1+optimalPathFromup )
+                    optimalPathFromUp = dp[i-1][j]
+                    dp[i][j] = max(dp[i][j],c2-c1,c2-c1+optimalPathFromUp)
+                    
                 ans = max(ans,dp[i][j])
+                
         return ans
