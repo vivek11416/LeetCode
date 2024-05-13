61b83daeddb7243a376ae1cffdb5964810be69d1
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-05-13 23:44:18 +0530

    Time: 1250 ms (100.00%), Space: 29.9 MB (100.00%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
@@ -18,13 +18,13 @@ class Solution:
                 if j-1 >= 0:
                     c2 = grid[i][j]
                     c1 = grid[i][j-1]
-                    maxScoreFromLeft  = dp[i][j-1]
+                    # maxScoreFromLeft  = dp[i][j-1]
                     dp[i][j] = max(c2-c1,c2-c1+dp[i][j-1])
                     
                 if i-1 >= 0:
                     c2 = grid[i][j]
                     c1 = grid[i-1][j]
-                    maxScoreFromTop  = dp[i-1][j]
+                    # maxScoreFromTop  = dp[i-1][j]
                     dp[i][j] = max(dp[i][j],c2-c1,c2-c1+dp[i-1][j])
                     
                 ans = max(ans,dp[i][j])
