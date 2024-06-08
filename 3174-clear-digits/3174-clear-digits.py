87df779f5854e9379f9982297023d9d1d496f7ed
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-06-08 23:15:58 +0530

    Time: 40 ms (100.00%), Space: 16.5 MB (100.00%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        heap = []
+        s  =list(s)
+        
+        for i,v in enumerate(s):
+            if v.isalpha():
+                heapq.heappush(heap,-i)
+            
+            else:
+                print('hello')
+                heapq.heappop(heap)
+                
+        ansList = []
+        while heap:
+            j = heapq.heappop(heap)
+            ansList.append(s[-j])
+        
+        return "".join(ansList[::-1])
+                
