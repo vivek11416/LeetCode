cb6d5ef59ea69f26da50729c9f3d3da0aaf3f06d
vivek kumar pal <42150230+vivek11416@users.noreply.github.com>
2024-06-09 12:16:00 +0530

    Time: 230 ms (56.97%), Space: 21.5 MB (55.53%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remMap = defaultdict(int)
+        remMap[0]=1
+        ans=0
+        preSum = 0
+        
+        for i,num in enumerate(nums):
+            preSum += num
+            rem = preSum % k
+            
+            if rem in remMap:
+                ans += remMap[rem]
+            
+            remMap[rem] += 1
+            
+        return ans
+            
+     
+            
+        
